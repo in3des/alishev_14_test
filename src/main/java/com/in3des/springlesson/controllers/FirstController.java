4eabd7cbd3caaa2a7836783f98ef969637d392f2
in3des <62700347+in3des@users.noreply.github.com>
2021-06-26 01:25:00 +0400

    new pages - first & second

---
+++ src/main/java/com/in3des/springlesson/controllers/FirstController.java
@@ -0,0 +1,18 @@
+package com.in3des.springlesson.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String helloPage() {
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodByePage() {
+        return "first/goodbye";
+    }
+}
