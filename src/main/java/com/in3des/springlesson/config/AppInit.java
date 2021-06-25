38e44612d7b6d93cf9f56138fa24af7fbaa0b251
in3des <62700347+in3des@users.noreply.github.com>
2021-06-26 01:00:32 +0400

    delete xml add @ controller

---
+++ src/main/java/com/in3des/springlesson/config/AppInit.java
@@ -0,0 +1,20 @@
+package com.in3des.springlesson.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
