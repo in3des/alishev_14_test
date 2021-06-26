faa8f45f4db23e56cb44d98ab63b8252d9e0cd5a
in3des <62700347+in3des@users.noreply.github.com>
2021-06-26 16:38:39 +0400

    add calc

--- src/main/java/com/in3des/springlesson/controllers/FirstController.java
+++ src/main/java/com/in3des/springlesson/controllers/FirstController.java
@@ -1,7 +1,10 @@
 package com.in3des.springlesson.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
@@ -14,5 +17,33 @@ public class FirstController {
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calc")
+    public String calculator(@RequestParam(value = "a") int a, @RequestParam(value = "b") int b, @RequestParam(value = "operator") String s, ModelMap model) {
+
+        double result;
+
+        switch (s) {
+            case "sum":
+                result = a + b;
+                break;
+            case "minus":
+                result = a - b;
+                break;
+            case "mult":
+                result = a * b;
+                break;
+            case "div":
+                result = a / (double) b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        model.addAttribute("result", result);
+
+        return "first/calc";
     }
 }
